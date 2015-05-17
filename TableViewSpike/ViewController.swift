c589733bdaef61e651043478f8e853790b9968ab
Alexis Gallagher <alexis@alexisgallagher.com>
2015-05-16 21:02:30 -0700

    Initial Commit

---
+++ TableViewSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewSpike
+//
+//  Created by Alexis Gallagher on 2015-05-16.
+//  Copyright (c) 2015 Bloom Filter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
