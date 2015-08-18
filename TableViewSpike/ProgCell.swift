ee5966c4d24e20f767299489798ef791dc2ce1b8
Alexis Gallagher <alexis@alexisgallagher.com>
2015-08-17 22:29:14 -0700

    comment

--- TableViewSpike/ProgCell.swift
+++ TableViewSpike/ProgCell.swift
@@ -53,47 +53,65 @@ class ProgCell : UITableViewCell
     Parenthetical Discussion of Constraint Errors 
     Due to the Notorious "UIView-Encapsulated-Layout-Height" Constraint
 
-    The constraint below additionally requires a minimum height of 50, irrespective of the cell's
-    content. As a result, if you activate this constraint, then the cell's constraints become 
-    incompatible with any _additional_ constraint requiring the cell height to be smaller than 50, 
-    such as for it to be 44.
+    The constraints below allow you to configure the cell to add an additional requirement of
+    a minimum height of 50, to cell, its contentview, or the label. As a result, if you activate
+    any of these, then the cell's constraints become incompatible with any _additional_
+    constraint requiring the cell height to be smaller than 50, such as for it to be 44.
     
     Why is this interesting? When a UITableView using auto-sizing logic first runs, it initializes
-    the cell, calls `tableView(_:cellForRowAtIndexPath:)` in order to populate the cell with content, and
-    then calls `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)`
+    the cell, calls `tableView(_:cellForRowAtIndexPath:)` in order to populate the cell with content, 
+    and then calls the cell's 
+    `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)`
     in order to allow the cell to express its desired height given the width of the table view.
     
-    But at the start of this call, the UITableViewCell.contentView begins with constraints for its 
+    But at the start of this call, the UITableViewCell begins with constraints for its
     default width (the screen width) and height (44 points!). This latter constraint is called
-    `UIView-Encapsulated-Layout-Height`. If the constraints you have defined on the cell are
-    incompaitble with this constraint requiring height==44, then the normal call to
+    `UIView-Encapsulated-Layout-Height`. Sometimes it appears on the cell, sometimes on the 
+    contentView. I don't know what rules decides this. If the constraints you have defined on the 
+    cell are incompiatble with this constraint requiring height==44, then the normal call to
     `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)` will emit 
     error messages into the console regarding unsatisfiable constraints.
 
-    However, these errors are _transient_. On its own, the UITV will later use the value your cell's
-    provides for its desired height (defined via its own Auto Layout constraints, and revealed via 
+    However, these errors are _transient_. On its own, the UITV will later use the value that AL
+    calculates for your cell's desired height (as revealed via
     the call to `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)`)
-    in order to reset the `UIView-Encapsulated-Layout-Height` to a value that is compatible with
+    and then reset the `UIView-Encapsulated-Layout-Height` to a value that is compatible with
     your cell's constraints and contents.
 
     So what?
     
-    To avoid seeing these spurious error messages, you have a few options:
-    1. Configure your cell's constraints so that they are compatible with it having a required 
-       height of 44. For instance, you could reduce to 999 the priority of the spacer constraint
-       connecting the bottom of the contentView to your bottom-most subview within the contentView.
-       This makes it "legitimate" for your contentView's subviews to poke out beyond the bottom of
-       your subview, and makes a transient constraint that contentView.height==44 satisfiable.
+    To avoid seeing these spurious error messages, you should configure your cell's constraints so 
+    that they are compatible with it having a required height of 44. For instance, you could reduce 
+    to 999 the priority of the spacer constraint connecting the bottom of the contentView to your 
+    bottom-most subview within the contentView. This makes it "legitimate" for your contentView's 
+    subviews to poke out beyond the bottom of your subview, and makes a transient constraint that 
+    contentView.height==44 satisfiable.
     
     The part I don't understand:
+    1. How does UIKit decide where to put this spurious, transient constraint?
+    2. If the `UIView-Encapsulated-Layout-Height` constraint starts out on the contentView, then how
+       does `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)`
+       determine what height the contentView would want to be in the absence of this constraint?
+    3. Why is this so complicated?
     
-    If the `UIView-Encapsulated-Layout-Height` constraint starts out on the contentView, then how
-    does `systemLayoutSizeFittingSize(:withHorizontalFittingPriority:verticalFittingPriority:)` 
-    determine what height the contentView would want to be in the absence of this constraint??
+    FINDINGS SUMMARY:
+    1. height44 constraint invisible outside call to systemLayoutSizeFittingSize(...)
+    2. height44 constraint is initalled on the cell not the contentView (in _this_ example...)
+    3. non44 constraint on cell => unsatisfiabilility warning
+    4. non44 constraint on cell.contentView => no unsatisfiability warning
+    5. non44 constraint on cell.contentView.subviews => no unsatisfiability warning
     
     */
-    let minimumHeightConstraint = NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .GreaterThanOrEqual, toItem: nil, attribute: .NotAnAttribute, multiplier: 0, constant: 50)
-    minimumHeightConstraint.active = false
+    func height50ConstraintOnView(v:UIView) -> NSLayoutConstraint {
+      return NSLayoutConstraint(item: v, attribute: .Height, relatedBy: .GreaterThanOrEqual, toItem: nil, attribute: .NotAnAttribute, multiplier: 0, constant: 50)
+    }
+    let minimumCellHeightConstraint = height50ConstraintOnView(self)
+    let minimumCellContentViewHeightConstraint = height50ConstraintOnView(self.contentView)
+    let minimumLabelHeightConstraint = height50ConstraintOnView(bodyLabel)
+
+    minimumCellHeightConstraint.active = false
+    minimumCellContentViewHeightConstraint.active = false
+    minimumLabelHeightConstraint.active = false
   }
   
   override func layoutSubviews() {
@@ -106,7 +124,11 @@ class ProgCell : UITableViewCell
   }
   
   override func systemLayoutSizeFittingSize(targetSize: CGSize, withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority, verticalFittingPriority: UILayoutPriority) -> CGSize {
+//    NSLog("existing constraints on cell.contentView.label: %@", bodyLabel.constraintsAffectingLayoutForAxis(.Vertical))
+//    NSLog("existing constraints on cell.contentView: %@", self.contentView.constraintsAffectingLayoutForAxis(.Vertical))
+//    NSLog("existing constraints on cell: %@", self.constraintsAffectingLayoutForAxis(.Vertical))
     let result = super.systemLayoutSizeFittingSize(targetSize, withHorizontalFittingPriority: horizontalFittingPriority, verticalFittingPriority: verticalFittingPriority)
+    
     return result
   }
   
